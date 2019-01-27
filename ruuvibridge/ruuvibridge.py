d191e385a13435307984532e64273bed35a361fd
Juhani Hietikko <juhani.hietikko@reaktor.com>
2019-01-27 11:24:29 +0200

    Add more sensors and change to production metrics namespace

--- ruuvibridge/ruuvibridge.py
+++ ruuvibridge/ruuvibridge.py
@@ -13,6 +13,18 @@ tags = {
     },
     'C6:57:4E:37:1E:66': {
         'name': 'Fridge'
+    },
+    'E4:C5:67:4E:6B:37': {
+        'name': 'Small Bedroom'
+    },
+    'D3:08:3A:26:44:84': {
+        'name': 'Bathroom'
+    },
+    'DD:18:D0:A1:50:D7': {
+        'name': 'Living Room'
+    },
+    'F2:F6:4B:B0:A7:27': {
+        'name': 'Garden'
     }
 }
 
@@ -39,7 +51,7 @@ def process_sample(sample):
         now = datetime.now()
         if now - tag['last_upload'] >= timedelta(minutes=1):
             cloudwatch.put_metric_data(
-                Namespace='Test5',
+                Namespace='HomeEnvironV1',
                 MetricData=[
                     metricdata(tag, sample, 'Temperature', 'temperature'),
                     metricdata(tag, sample, 'Pressure', 'pressure'),
