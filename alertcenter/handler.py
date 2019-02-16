760d2bcb74d55553f4f287c97de7bda10559ec21
Juhani Hietikko <juhani.hietikko@reaktor.com>
2019-02-16 15:30:37 +0200

    Fix saving of door sensor data in dynamodb

--- alertcenter/handler.py
+++ alertcenter/handler.py
@@ -6,20 +6,32 @@ from datetime import datetime, timedelta
 
 dynamo = boto3.client('dynamodb')
 
+
+def dynamo_list(list):
+    return [{'N': str(val)} for val in list]
+
+
 def sensor_motion(event, context):
-    print('Timestamp, ms: ' + math.floor(1000 * datetime.utcnow().timestamp()))
     req_body = json.loads(event['body'])
     tag = req_body['tag']
     timestamp_ms = math.floor(1000 * datetime.utcnow().timestamp())
     expires = math.floor((datetime.utcnow() + timedelta(days=30)).timestamp())
+
+    acc_total = dynamo_list(req_body['acceleration_samples']['total'])
+    acc_x = dynamo_list(req_body['acceleration_samples']['x'])
+    acc_y = dynamo_list(req_body['acceleration_samples']['y'])
+    acc_z = dynamo_list(req_body['acceleration_samples']['z'])
 
     dynamo.put_item(
         TableName='motion',
         Item={
             'Tag': {'S': tag},
             'EventTime': {'N': str(timestamp_ms)},
-            'AccelerationSamples': {'M': json.dumps(req_body['acceleration_samples'])},
-            'Expires': {'n': str(expires)}
+            'AccelerationSamplesTotal': {'L': acc_total},
+            'AccelerationSamplesX': {'L': acc_x},
+            'AccelerationSamplesY': {'L': acc_y},
+            'AccelerationSamplesZ': {'L': acc_z},
+            'Expires': {'N': str(expires)}
         }
     )
     response = {
