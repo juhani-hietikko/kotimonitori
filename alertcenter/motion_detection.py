4bdff916b6e07126148f5af9727ed1cbbd46bac0
Juhani Hietikko <juhani.hietikko@reaktor.com>
2019-02-16 21:57:48 +0200

    Add simple authorization to motion detection api
    IFTTT webhook doesn't allow defining request headers, so we can't
    use X-Api-Key header for easy API Gateway authorization as with
    door sensor API.

--- alertcenter/motion_detection.py
+++ alertcenter/motion_detection.py
@@ -5,9 +5,19 @@ import boto3
 from datetime import datetime, timedelta
 
 dynamo = boto3.client('dynamodb')
+ssm = boto3.client('ssm')
+api_key = ssm.get_parameter(Name='motion-detector-api-key', WithDecryption=True)['Parameter']['Value']
 
 def handle(event, context):
     req_body = json.loads(event['body'])
+
+    req_api_key = req_body.get('apikey')
+    if req_api_key != api_key:
+        return {
+            'statusCode': 403,
+            'body': 'Forbidden'
+        }
+
     detector = req_body['detector']
     timestamp_ms = math.floor(1000 * datetime.utcnow().timestamp())
     expires = math.floor((datetime.utcnow() + timedelta(days=30)).timestamp())
@@ -20,8 +30,7 @@ def handle(event, context):
             'Expires': {'N': str(expires)}
         }
     )
-    response = {
+    return {
         'statusCode': 200,
         'body': 'Registered motion from detector ' + detector
     }
-    return response
